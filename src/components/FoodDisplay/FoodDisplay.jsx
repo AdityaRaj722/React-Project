a9af4cb130591029c913847118ce9713412b537c
AdityaRaj722 <aditya0722004@gmail.com>
2024-04-25 15:26:38 +0530

    React CA

---
+++ src/components/FoodDisplay/FoodDisplay.jsx
@@ -0,0 +1,22 @@
+import React, { useContext } from 'react'
+import './FoodDisplay.css'
+const FoodDisplay = ({category}) => {
+    const{food_list}=useContext(StoreContext)
+  return (
+    <div className='food-display' id='food-display'>
+        <h2>Top dishes near you</h2>
+      <div className="food-display-list">
+        {food_list.map((item,index)=>{
+            // eslint-disable-next-line no-cond-assign
+            if(category==="All" || category===item.category){
+                return <FoodItem key={index} id={item._id} name={item.name} description={item.description} price={item.price} image={item.image}/>
+            }
+        })}
+      </div>
+    </div>
+  )
+}
+import './FoodDisplay.css'
+import { StoreContext } from '../../context/StoreContext'
+import FoodItem from '../FoodItem/FoodItem'
+export default FoodDisplay
